5f2fac0526939e139bc10e147cdfd1240a834ab1
KilianKlaiber <kilianklaiber@gmail.com>
2024-11-13 09:58:30 +0100

    gui.py renamed to main.py

--- message.py
+++ message.py
@@ -1,9 +1,27 @@
 import imaplib
 import os
+from email.message import EmailMessage
 
 EMAILADDRESS = os.getenv("EMAILADRESS")
 EMAILPASSWORD = os.getenv("EMAILPASSWORD")
 
 
-with imaplib.IMAP4_SSL("imap.gmail.com", 993) as connection:
-    connection.login(EMAILADDRESS,EMAILPASSWORD)
+def connect() -> object:
+
+    with imaplib.IMAP4_SSL("imap.gmail.com", 993) as connection:
+        connection.login(EMAILADDRESS,EMAILPASSWORD)
+        
+        return connection
+
+
+def create_message(To: str, content: str, subject: str="Shopping List") -> object:
+    
+    message = EmailMessage()
+
+    message("From") = "kilianklaiber@gmail.com"
+    message("To") = To
+    message('Subject') = subject
+
+    message.set_content(content)
+    
+    return message
